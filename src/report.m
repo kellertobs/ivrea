22e4f320ee1a496738f1d95064d59af598c4146e
Tobias Keller <kellertobs@gmail.com>
2021-03-04 09:21:41 +0100

    minor update to avoid reporting 0th iteration residual

--- src/report.m
+++ src/report.m
@@ -6,7 +6,7 @@ resnorm = norm(dU(:),2)./(1e-16+norm(W(:)+WBG(:),2)) ...
         + norm(dP(:),2)./(1e-16+norm(P(:),2)) ...
         + norm(df(:),2)./(1e-16+norm(f(:),2));
 
-if it<=nup || resnorm>resnorm0; resnorm0 = resnorm + 1e-32; end  % reset reference residual
+if it<=max(100,nup) || resnorm>resnorm0; resnorm0 = resnorm + 1e-32; end  % reset reference residual
  
 % get total mass error
 if bnchmrk
@@ -14,7 +14,7 @@ if bnchmrk
         + norm((W(:)-W_mms(:)),2)./(1e-16+norm(W_mms(:),2)) ...
         + norm((P(:)-P_mms(:)),2)./(1e-16+norm(P_mms(:),2));
 else
-    if step<=1 && it<=nup; fmass0 = sum(f(:)); end
+    if step<=1 && it<=max(100,nup); fmass0 = sum(f(:)); end
     fmass = sum(f(:));
     err   = abs(fmass-fmass0)/fmass0;
 end
@@ -34,13 +34,13 @@ end
 
 % plot convergence of outer iterations
 if plot_cv
-    figure(100); if it==0; clf; else; hold on; end
+    figure(100); if it<=max(100,nup); clf; else; hold on; end
     plot(it,log10(resnorm),'r.','MarkerSize',15); box on; axis tight;
     
-    figure(101);
-    subplot(1,3,1); imagesc(-dWi); axis equal tight; box on; colorbar; colormap(ocean);
-    subplot(1,3,2); imagesc( dUi); axis equal tight; box on; colorbar; colormap(ocean);
-    subplot(1,3,3); imagesc( dPi); axis equal tight; box on; colorbar; colormap(ocean); 
+%     figure(101);
+%     subplot(1,3,1); imagesc(-dWi); axis equal tight; box on; colorbar; colormap(ocean);
+%     subplot(1,3,2); imagesc( dUi); axis equal tight; box on; colorbar; colormap(ocean);
+%     subplot(1,3,3); imagesc( dPi); axis equal tight; box on; colorbar; colormap(ocean); 
     
     drawnow;
 end
