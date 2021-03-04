1757cfb3ea2ba7bf979e541510f96301703eeda4
Tobias Keller <kellertobs@gmail.com>
2021-03-04 09:22:00 +0100

    updated demo run script

--- usr/run_demo.m
+++ usr/run_demo.m
@@ -1,17 +1,17 @@
 clear; close all;                % #ok<*NASGU> 
 
 % set run parameters
-runID    = 'demo';               % run identifier
-restart  =  12;                   % restart from file (0: new run; <1: restart from last; >1: restart from specified frame)
-nop      =  10;                  % output frame plotted/saved every 'nop' time steps
+runID    = 'isotope';             % run identifier
+restart  =  0;                   % restart from file (0: new run; <1: restart from last; >1: restart from specified frame)
+nop      =  1;                  % output frame plotted/saved every 'nop' time steps
 plot_op  =  1;                   % switch on (1) to display results
 save_op  =  1;                   % switch on (1) to save output to file
 plot_cv  =  1;                   % switch on (1) to live plot iterative convergence
 bnchmrk  =  0;                   % switch on (1) to run manufactured solution benchmark
 
 % set model domain parameters
-L        =  50;                  % domain dimension
-N        =  500 + 2;             % number of grid points in z-direction (incl. 2 ghosts)
+L        =  40;                  % domain dimension
+N        =  200 + 2;             % number of grid points in z-direction (incl. 2 ghosts)
 h        =  L/(N-2);             % grid spacing
 
 % set model timing parameters
@@ -24,30 +24,37 @@ Es       =  0;                   % matrix viscosity activation energy
 Ef       =  0;                   % melt viscosity activation energy
 
 % stress control parameters
-Pu       =  0;                   % ratio of pure-shear stress to buoyancy pressure
+Pu       = -0.25;                % ratio of pure-shear stress to buoyancy pressure
 Si       =  0;                   % ratio of simple-shear stress to buoyancy pressure
 B        =  1;                   % ratio of buoyancy pressure to tensile strength
 
 % thermo-chemical model parameters
-Tc       =  0.20;                % deep crustal temperature (top boundary)
+Tc       =  0.10;                % deep crustal temperature (top boundary)
 T0       =  0.40;                % initial temperature
-T1       = -0.01;                % amplitude of random noise   
-T2       = -0.00;                % amplitude of gaussian
-D        =  L/3;                 % thermal boundary layer thickness
-PeT      =  10;                  % thermal Peclet number [u0.h0/kappaT0]
-PeC      =  100;                 % major element Peclet number [u0.h0/Dc0]
-St       =  5;                   % Stefan number [cp0.dT0/L0]
-MAJ0     =  0.20;                % initial major element composition
-MAJ1     =  0.10;                % amplitude of random noise   
+T1       =  0.00;                % amplitude of random noise   
+T2       =  0.02;                % amplitude of gaussian
+D        =  0.4*L;               % thermal boundary layer thickness
+PeT      =  2.0;                 % thermal Peclet number [u0.h0/kappaT0]
+PeC      =  200;                 % major element Peclet number [u0.h0/Dc0]
+St       =  3.0;                 % Stefan number [cp0.dT0/L0]
+MAJ0     =  0.17;                % initial major element composition
+MAJ1     =  0.025;               % amplitude of random noise   
 MAJ2     =  0.00;                % amplitude of gaussian
-TRC0     =  1;                   % initial trace element composition
-TRC1     =  0.1;                 % amplitude of random noise   
-TRC2     =  0.2;                 % amplitude of gaussian
-KTRC     =  0.01;                % trace element partitioning coefficient
-SIS0     =  1;                   % initial stable isotope composition
-SIS1     =  0.1;                 % amplitude of random noise   
-SIS2     =  0.2;                 % amplitude of gaussian
-Da       =  200;                 % Dahmköhler number [t0/tr0]
+TRI0     =  1.0;                 % initial incompatible trace element composition
+TRI1     =  0.0;                 % amplitude of random noise   
+TRI2     =  0.0;                 % amplitude of gaussian
+KTRI     =  0.01;                % incompatible trace element partitioning coefficient
+TRC0     =  1.0;                 % initial compatible trace element composition
+TRC1     =  0.0;                 % amplitude of random noise   
+TRC2     =  0.0;                 % amplitude of gaussian
+KTRC     =  100;                 % compatible trace element partitioning coefficient
+ISS0     =  1.0;                 % initial solid stable isotope composition
+ISS1     =  0.0;                 % amplitude of random noise   
+ISS2     =  1.0;                 % amplitude of gaussian
+ISF0     =  1.0;                 % initial fluid stable isotope composition
+ISF1     =  0.0;                 % amplitude of random noise   
+ISF2     =  1.0;                 % amplitude of gaussian
+Da       =  300;                 % Dahmköhler number [t0/tr0]
 PhDg     =  4.0;                 % Phase diagram scaling factor (> 1)
 perCf    =  0.8;                 % peritectic liquidus composition
 perCs    =  0.7;                 % peritectic solidus  composition
@@ -57,23 +64,24 @@ smz      =  (N/50)^2;            % smoothness of initial random noise in z-direc
 wx       =  L/5;                 % horizontal half-width of gaussian
 wz       =  L/5;                 % vertical half-width of initial gaussian
 xpos     =  0;                   % x-position of initial gaussian (0 = middle of domain)
-zpos     = -L/2;                 % z-position of initial gaussian (0 = middle of domain)
+zpos     = -L;                   % z-position of initial gaussian (0 = middle of domain)
 
 % set numerical model parameters
-nup      =  25;                  % nonlinear coefficients, residual norms updated every 'nup' iterations
-CFL      =  0.75;                % (physical) time stepping courant number (multiplies stable step) [0,1]
-ADVN     =  'FRM';               % advection scheme ('UPW2', 'UPW3', or 'FRM')
+nup      =  50;                  % nonlinear coefficients, residual norms updated every 'nup' iterations
+CFL      =  1.0;                 % (physical) time stepping courant number (multiplies stable step) [0,1]
+ADVN     =  'FLXDIV';            % advection scheme ('FROMM','FLXDIV')
 theta    =  0.50;                % time-stepping scheme selector (1=BE, 1/2=CN, 0=FE)
 rtol     =  1e-6;                % outer its relative tolerance
 atol     =  1e-6;                % outer its absolute tolerance
 minit    =  500;                 % minimum solver iterations
-maxit    =  5e3;                 % maximum solver iterations
+maxit    =  4e3;                 % maximum solver iterations
 alpha    =  0.90;                % inner its step size (fraction of stable step) [0,1]
-beta     =  0.75;                % iterative damping parameter (fraction of previous step) [0,1]
-delta    =  0.99;               % iterative relaxation for rheology updates [0,1]
-kappa    =  4.0;                % regularisation of eIIvp for failure [0,1]
-etamin   =  0.03;                % minimum viscosity for regularisation
-etamax   =  1e+2;                % maximum viscosity for stability
+beta     =  0.80;                % iterative damping parameter (fraction of previous step) [0,1]
+gamma    =  0.99;                % iterative relaxation for rheology updates [0,1]
+kappa    =  2.00;                % regularisation of eIIvp for failure [0,1]
+etamin   =  0.003;               % minimum viscosity for regularisation
+etamax   =  3e+3;                % maximum viscosity for stability
+flim     =  3e-4;                % limit melt fraction in coefficients for stability
 
 % create output directory
 if ~isfolder(['../out/',runID])
