41901cf265ec2fa0f5e14e521030a081fb31cbce
Tobias Keller <kellertobs@gmail.com>
2021-03-25 08:28:08 +0100

    updated run scripts with few new parameters

--- usr/run_meltflush.m
+++ usr/run_meltflush.m
@@ -1,8 +1,8 @@
 clear; close all;                % #ok<*NASGU> 
 
 % set run parameters
-runID    = 'demo';               % run identifier
-restart  =  0;                   % restart from file (0: new run; <1: restart from last; >1: restart from specified frame)
+runID    = 'meltflush';          % run identifier
+restart  =  5;                   % restart from file (0: new run; <1: restart from last; >1: restart from specified frame)
 nop      =  10;                  % output frame plotted/saved every 'nop' time steps
 plot_op  =  1;                   % switch on (1) to display results
 save_op  =  1;                   % switch on (1) to save output to file
@@ -10,7 +10,7 @@ plot_cv  =  1;                   % switch on (1) to live plot iterative converge
 bnchmrk  =  0;                   % switch on (1) to run manufactured solution benchmark
 
 % set model domain parameters
-L        =  40;                  % domain dimension
+L        =  20;                  % domain dimension
 N        =  200 + 2;             % number of grid points in z-direction (incl. 2 ghosts)
 h        =  L/(N-2);             % grid spacing
 
@@ -24,28 +24,28 @@ Si       =  0;                   % ratio of simple-shear stress to buoyancy pres
 B        =   1/2;                % ratio of buoyancy pressure to tensile strength
 
 % set model rheology parameters
-n        =  0.75;                % non-Newtonian shear viscosity powerlaw
-lambda   =  30;                  % exponential melt weakening factor
+n        =  0.66;                % non-Newtonian shear viscosity powerlaw
+lambda   =  35;                  % exponential melt weakening factor
 Es       =  10;                  % matrix viscosity activation energy
 Ef       =  5;                   % melt viscosity activation energy
 RHEAL    =  1;                   % damage healing rate
-KDMG     =  10;                  % damage permeability enhancement factor
 YDMG     =  0.2;                 % damage yield stress reduction factor
+KDMG     =  10;                  % damage permeability enhancement factor
 Pc       =  20;                  % deep crustal pressure (top boundary)
 
 % thermo-chemical model parameters
-Tc       =  0.20;                % deep crustal temperature (top boundary)
+Tc       =  0.485;               % deep crustal temperature (top boundary)
 T0       =  0.50;                % initial temperature
 T1       =  0.00;                % amplitude of random noise   
-T2       =  0.01;                % amplitude of gaussian
-D        =  0.3*L;               % thermal boundary layer thickness
+T2       =  0.00;                % amplitude of gaussian
+D        =  0.6*L;               % thermal boundary layer thickness
 PeT      =  1.0;                 % thermal Peclet number [u0.h0/kappaT0]
 PeC      =  100;                 % major element Peclet number [u0.h0/Dc0]
 St       =  3.0;                 % Stefan number [cp0.dT0/L0]
-MAJ0     =  0.10;                % initial major element composition
+MAJ0     =  0.15;                % initial major element composition
 MAJ1     =  0.025;               % amplitude of random noise   
 MAJ2     = -0.0;                 % amplitude of gaussian
-Da       =  100;                 % Dahmköhler number [t0/tr0]
+Da       =  200;                 % Dahmköhler number [t0/tr0]
 PhDg     =  4.0;                 % Phase diagram scaling factor (> 1)
 perCf    =  0.7;                 % peritectic liquidus composition
 perCs    =  0.6;                 % peritectic solidus  composition
@@ -88,13 +88,13 @@ ISF2     =  1.0;                 % amplitude of gaussian
 
 % set numerical model parameters
 nup      =  50;                  % nonlinear coefficients, residual norms updated every 'nup' iterations
-CFL      =  0.50;                % (physical) time stepping courant number (multiplies stable step) [0,1]
+CFL      =  0.5;                 % (physical) time stepping courant number (multiplies stable step) [0,1]
 theta    =  0.50;                % time-stepping scheme selector (1=BE, 1/2=CN, 0=FE)
 rtol     =  1e-6;                % outer its relative tolerance
 atol     =  1e-6;                % outer its absolute tolerance
 minit    =  500;                 % minimum solver iterations
 maxit    =  5e3;                 % maximum solver iterations
-alpha    =  0.95;                % inner its step size (fraction of stable step) [0,1]
+alpha    =  0.50;                % inner its step size (fraction of stable step) [0,1]
 beta     =  0.80;                % iterative damping parameter (fraction of previous step) [0,1]
 gamma    =  0.99;                % iterative relaxation for rheology updates [0,1]
 kappa    =  2.00;                % regularisation of eIIvp for failure [0,1]
