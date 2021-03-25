056bf0f79c82533c15fbe417a75e87ca480e72c7
Tobias Keller <kellertobs@gmail.com>
2021-03-25 08:27:06 +0100

    remove obsolete mass error tracking, add fail safe to catch diverging solver

--- src/report.m
+++ src/report.m
@@ -6,40 +6,27 @@ resnorm = norm(dU(:),2)./(1e-16+norm(W(:)+WBG(:),2)) ...
         + norm(dP(:),2)./(1e-16+norm(P(:),2));
 
 if it<=max(100,nup) || resnorm>resnorm0; resnorm0 = resnorm + 1e-32; end  % reset reference residual
- 
-% get total mass error
-if bnchmrk
-    err = norm((U(:)-U_mms(:)),2)./(1e-16+norm(U_mms(:),2)) ...
-        + norm((W(:)-W_mms(:)),2)./(1e-16+norm(W_mms(:),2)) ...
-        + norm((P(:)-P_mms(:)),2)./(1e-16+norm(P_mms(:),2));
-else
-    if step<=1 && it<=max(100,nup); fmass0 = sum(f(:)); end
-    fmass = sum(f(:));
-    err   = abs(fmass-fmass0)/fmass0;
-end
 
 % report iterations
 if     it >=  0  && it <  10
-    fprintf(1,'    ---  it =      %d;   abs res = %4.4e;   rel res = %4.4e;   err = %4.4e \n',it,resnorm,resnorm/resnorm0,err);
+    fprintf(1,'    ---  it =      %d;   abs res = %4.4e;   rel res = %4.4e \n',it,resnorm,resnorm/resnorm0);
 elseif it >= 10  && it < 100
-    fprintf(1,'    ---  it =     %d;   abs res = %4.4e;   rel res = %4.4e;   err = %4.4e \n',it,resnorm,resnorm/resnorm0,err);
+    fprintf(1,'    ---  it =     %d;   abs res = %4.4e;   rel res = %4.4e \n',it,resnorm,resnorm/resnorm0);
 elseif it >= 100 && it < 1000
-    fprintf(1,'    ---  it =    %d;   abs res = %4.4e;   rel res = %4.4e;   err = %4.4e \n',it,resnorm,resnorm/resnorm0,err);
+    fprintf(1,'    ---  it =    %d;   abs res = %4.4e;   rel res = %4.4e \n',it,resnorm,resnorm/resnorm0);
 elseif it >= 1000 && it < 10000
-    fprintf(1,'    ---  it =   %d;   abs res = %4.4e;   rel res = %4.4e;   err = %4.4e \n',it,resnorm,resnorm/resnorm0,err);
+    fprintf(1,'    ---  it =   %d;   abs res = %4.4e;   rel res = %4.4e \n',it,resnorm,resnorm/resnorm0);
 elseif it >= 10000
-    fprintf(1,'    ---  it =  %d;   abs res = %4.4e;   rel res = %4.4e;   err = %4.4e \n',it,resnorm,resnorm/resnorm0,err);
+    fprintf(1,'    ---  it =  %d;   abs res = %4.4e;   rel res = %4.4e;   err = %4.4e \n',it,resnorm,resnorm/resnorm0);
 end 
+
+if any(isnan([U(:);W(:);P(:);f(:)]))
+    error('!!! Solver diverged !!!')
+end
 
 % plot convergence of outer iterations
 if plot_cv
     figure(100); if it<=max(100,nup); clf; else; hold on; end
     plot(it,log10(resnorm),'r.','MarkerSize',15); box on; axis tight;
-    
-%     figure(101);
-%     subplot(1,3,1); imagesc(-dWi); axis equal tight; box on; colorbar; colormap(ocean);
-%     subplot(1,3,2); imagesc( dUi); axis equal tight; box on; colorbar; colormap(ocean);
-%     subplot(1,3,3); imagesc( dPi); axis equal tight; box on; colorbar; colormap(ocean); 
-    
     drawnow;
 end
