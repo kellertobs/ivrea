94f00ea1d663db13bcfa9de4d53f063d96118025
Tobias Keller <kellertobs@gmail.com>
2021-05-26 08:20:05 +0200

    extend advection to include upwind schemes

--- src/flxdiv.m
+++ src/flxdiv.m
@@ -5,6 +5,12 @@ wm = w(1:end-1,2:end-1);
 up = u(2:end-1,2:end  );
 um = u(2:end-1,1:end-1);
 
+vx  = (up+um)./2;
+vz  = (wp+wm)./2;
+
+vxp = max(vx,0); vxm = min(vx,0);
+vzp = max(vz,0); vzm = min(vz,0);
+
 Div_v = diff(w(:,2:end-1),1,1)./h + diff(u(2:end-1,:),1,2)./h;
 
 agh                    = zeros(size(a)+2);
@@ -21,6 +27,7 @@ aim = agh(3:end-2,2:end-3);  aimm = agh(3:end-2,1:end-4);
 
 switch scheme
     case 'FROMM'
+        
         Div_va = +     up .*(-(aipp-aip)./h./8 + (aip + acc)./h./2 + (acc-aim )./h./8) ...
                  - abs(up).*(-(aipp-aip)./h./8 + (aip - acc)./h./4 - (acc-aim )./h./8) ...
                  -     um .*(-(aip -acc)./h./8 + (acc + aim)./h./2 + (aim-aimm)./h./8) ...
@@ -29,9 +36,30 @@ switch scheme
                  - abs(wp).*(-(ajpp-ajp)./h./8 + (ajp - acc)./h./4 - (acc-ajm )./h./8) ...
                  -     wm .*(-(ajp -acc)./h./8 + (acc + ajm)./h./2 + (ajm-ajmm)./h./8) ...
                  + abs(wm).*(-(ajp -acc)./h./8 + (acc - ajm)./h./4 - (ajm-ajmm)./h./8);
+             
     case 'FLXDIV'
+        
         Div_va = ((ajp+acc)./2.*wp - (ajm+acc)./2.*wm)./h ...
                + ((aip+acc)./2.*up - (aim+acc)./2.*um)./h;
+           
+    case 'UPW2'
+        
+        axp   = (-3*acc+4*aip-aipp)/2/h;
+        axm   = ( 3*acc-4*aim+aimm)/2/h;
+        azp   = (-3*acc+4*ajp-ajpp)/2/h;
+        azm   = ( 3*acc-4*ajm+ajmm)/2/h;
+        
+        Div_va  = vxp.*axm + vxm.*axp + vzp.*azm + vzm.*azp + acc.*Div_v;
+        
+        
+    case 'UPW3'
+        
+        axp   = (-2*aim-3*acc+6*aip-aipp)/6/h;
+        axm   = ( 2*aip+3*acc-6*aim+aimm)/6/h;
+        azp   = (-2*ajm-3*acc+6*ajp-ajpp)/6/h;
+        azm   = ( 2*ajp+3*acc-6*ajm+ajmm)/6/h;
+        
+        Div_va  = vxp.*axm + vxm.*axp + vzp.*azm + vzm.*azp + acc.*Div_v;
 end
 
 if strcmp(type,'adv')
